915e193ae1c91e70b082e6b20be07e0ba882c3c3
Matheus Costa Francisco <37197065+kazankay@users.noreply.github.com>
2020-04-08 07:50:42 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 
 module.exports = {
-    connectionString: 'mongodb+srv://admin:masterkey@cluster0-birxs.gcp.mongodb.net/test?retryWrites=true&w=majority',
-    sendgridKey: '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',
+    connectionString: '',
+    sendgridKey: '',
     containerConnectionString: 'TDB'
 }
